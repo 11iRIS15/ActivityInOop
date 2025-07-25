9d843581959953382ff40e9b16c746eabb4ea345
Iris Atanante <atananteiris0@gmail.com>
2025-07-25 17:34:37 +0800

    Initial Commit

--- ActivityInOop/Program.cs
+++ ActivityInOop/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!,iloyouyou");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter price: "); //asked the user to input the price
+        float price = float.Parse(Console.ReadLine());
+
+        Console.Write("Enter quantity: "); //asked the user to input the quantity
+        int quantity = int.Parse(Console.ReadLine());
+
+        if (price > 0 && quantity > 0) //check if the price and the quantity is greater than 0
+        {
+            float total = price * quantity; //multiply it
+            Console.WriteLine($"Total cost: Php {total:F2}"); //display 
+        }
+        else
+        {
+            Console.WriteLine("Price and quantity must be positive."); //display when the value is out of range
+        }
+    }
+}
