7ee12de7805c1a8e40a051e5812072cfd60d6dbd
Iris Atanante <atananteiris0@gmail.com>
2025-07-25 17:51:05 +0800

    Initial Commit

--- ActivityInOop/Program.cs
+++ ActivityInOop/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!,iloyouyou");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter name: "); //asked user to input their name
+        string name = Console.ReadLine();
+
+        Console.Write("Enter age: "); //asked user to input their age
+        int age = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter height (in feet): "); ////asked user to input their heigth
+        float height = float.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Name: {name}, Age: {age}, Height: {height}"); //display all the information in one line
+    }
+}
+
